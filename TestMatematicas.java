559b1119862cb767161e10d51214cbabb188be21
Irene Beriain <irebercab@gmail.com>
2023-01-11 14:13:01 +0100

    Versión 0.0.1.

---
+++ TestMatematicas.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TestMatematicas {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("Introduce un número");
+		int numero = sc.nextInt();
+		if (Matematicas.esPar(numero) )
+			System.out.println("El numero es par");
+		else
+			System.out.println("El número es impar");
+		}
+	}
